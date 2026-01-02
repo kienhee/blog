f3dfd0208685e9dbaa1d473607ea34a767b26ff0
Trần Trung Kiên <kienhee.it@gmail.com>
2026-01-02 08:40:31 +0700

    Enhance post thumbnail interaction and styling
    - Added a clickable link around the post thumbnail image to open it in a gallery with Fancybox.
    - Improved hover effect on links within the post card for better user experience.
    - Updated SCSS to include transition effects for link opacity on hover.

--- resources/views/client/components/post/post-thumbnail.blade.php
+++ resources/views/client/components/post/post-thumbnail.blade.php
@@ -1,10 +1,14 @@
 {{-- Post Thumbnail --}}
 @if (isset($post->thumbnail) && $post->thumbnail)
     <div class="post-thumbnail">
-        <img src="{{ $post->thumbnail }}" 
-             alt="{{ $post->title ?? 'Thumbnail bài viết' }}" 
-             title="{{ $post->title ?? '' }}"
-             loading="lazy" />
+        <a href="{{ $post->thumbnail }}" 
+           data-fancybox="gallery" 
+           data-caption="{{ $post->title ?? 'Thumbnail bài viết' }}">
+            <img src="{{ $post->thumbnail }}" 
+                 alt="{{ $post->title ?? 'Thumbnail bài viết' }}" 
+                 title="{{ $post->title ?? '' }}"
+                 loading="lazy" />
+        </a>
     </div>
 @endif
 
