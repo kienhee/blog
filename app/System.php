419ba132f8560f4576b77fb3785be0fa524be7cc
Tran Trung Kien <75593578+kienhee@users.noreply.github.com>
2026-01-08 10:36:07 +0700

    ẩn font của code

--- app/System.php
+++ app/System.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Log;
 
 class System
 {
-    public static $version_view = 7; // Tăng view mỗi khi thay đổi css or js
+    public static $version_view = 8; // Tăng view mỗi khi thay đổi css or js
     /**
      * Get the admin asset URL with versioning.
      *
